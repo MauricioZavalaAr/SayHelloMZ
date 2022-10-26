05051f8ff1014511882b20eb7d330d3d81170c28
MauricioZavalaAr <mauricio.zavala.ar@gmail.com>
2022-10-25 20:26:38 -0500

    Mauricio commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿bool playAgain = true;
+bool validResponse = true;
+string UAnswer;
+while(playAgain == true) 
+{
+// start
+string name1;
+Console.WriteLine("What is your name?!");
+name1 = Console.ReadLine();
+Console.WriteLine($"Hello {name1}");
+// end
+validResponse = true;
+while(validResponse == true) 
+    {
+        Console.Write("Do you want to play again? (Y/N): "); 
+        UAnswer = Console.ReadLine().ToUpper();
+        if (UAnswer == "Y")
+        {
+            playAgain = true;
+            validResponse = false; 
+        }
+        else if (UAnswer == "N") 
+        {
+            playAgain = false; 
+            validResponse = false; 
+        }
+        else 
+        {
+            validResponse = true; 
+            playAgain = false; 
+            Console.WriteLine(UAnswer + " is not a valid response. Enter a response again");
+        }
+    }
+Console.WriteLine("Thank you for playing!"); 
+}
+
